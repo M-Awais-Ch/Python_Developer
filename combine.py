734f7582f72c9dbc24cea4495d6037b1071d9670
Awais <muhammadawaisidrees478@gmail.com>
2025-07-29 11:31:46 +0500

    add something

---
+++ combine.py
@@ -0,0 +1,13 @@
+a=[3331,45,8,56,4,3,7]
+for index, value in enumerate(a,start=1):
+    print(index,value)
+
+
+print(list(map(lambda x:x*2,a)))
+
+print(list(filter(lambda x:x%2==0,a)))
+
+
+from functools import reduce
+print(reduce(lambda x,y:x+y,a))
+print("hi")
